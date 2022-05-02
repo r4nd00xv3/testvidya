33def296f5c58414e34d8796ebd7400a649d292d
ramon andrade <ramon.andradeic@gmail.com>
2022-05-02 18:23:31 -0300

    ok Controle de licença por funcionalidades especiais de um produto ok Controle de Mídias do produto (logo, screenshots, vídeos, último changelog) OK Consumo de API externas para informações do CNPJ e/ou CEP OK Disponibilização via Swagger

--- src/main/java/com/example/vidyateste/repository/PesssoaJuridicaRepository.java
+++ src/main/java/com/example/vidyateste/repository/PesssoaJuridicaRepository.java
@@ -2,6 +2,7 @@ package com.example.vidyateste.repository;
 
 import com.example.vidyateste.model.PessoaJuridica;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -21,5 +22,18 @@ public interface PesssoaJuridicaRepository extends JpaRepository<PessoaJuridica,
 
 	@Query(value = "select u from PessoaJuridica u where u.datavalid <= current_date - 2")
 	List<PessoaJuridica> datavalidadelicenca();
+
+	/*@Modifying
+	@Query(value = "update PessoaJuridica  set licenca = true where  datavalid <= current_date - 0")
+	List<PessoaJuridica> desativalic();*/
+
+	@Modifying
+	@Query(value = "select u from PessoaJuridica u where u.")
+	List<PessoaJuridica> desativalic();
+
+
+	@Query(value = "select pj from PessoaJuridica pj where pj.cnpj = ?1")
+	public PessoaJuridica existeCnpjCadastrado(String cnpj);
+
 
 }
